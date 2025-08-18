7a3bee7cdaa7cbe0efa409e6c9796036879937ee
RoseMODA <rosema.ropa@gmail.com>
2025-08-17 23:46:39 -0300

    Add rosema-pos/src/main.jsx 1755485197977

---
+++ rosema-pos/src/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import './style.css'
+
+// Punto de entrada principal de la aplicación React
+// Se renderiza dentro de BrowserRouter para habilitar el routing
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
