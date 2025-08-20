00fc607decbc7ef5a670bc09bcc76ac5d9308ea2
CodeSandbox <support@codesandbox.io>
2025-08-20 14:26:27 +0000

    div

--- rosema-pos/src/components/SalesCart.jsx
+++ rosema-pos/src/components/SalesCart.jsx
@@ -245,6 +245,8 @@ const SalesCart = ({
       {/* Modal de descuento */}
       {showDiscountModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+          <div className="bg-white rounded-lg p-6 w-96 max-w-90vw">
+
             <h3 className="text-lg font-semibold mb-4">Aplicar Descuento</h3>
             
             <div className="space-y-4">
@@ -307,6 +309,7 @@ const SalesCart = ({
               </button>
             </div>
           </div>
+        </div>
       )}
 
       {/* Modal de venta en espera */}
